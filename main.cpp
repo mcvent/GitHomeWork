a0bd2196cb31de8a61ecc939ebb79978ba02498c
mcvent <mcventik@yandex.ru>
2025-03-16 13:06:30 +0400

    change in work func fib, started on 0

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include<stdio.h>
 
 int fib(int n) {
-    if (n <= 1) {
+    	if (n == 0) { 
+	return 0; }
+	if (n == 1) {
         return 1;
     }
     return fib(n - 1) + fib(n - 2);
@@ -9,7 +11,7 @@ int fib(int n) {
 
 int main() {
 	int n = 10; 
-    	int result = fib(n); 
+    	int result = fib(n-1); 
     	printf("fib(%d) = %d\n", n, result); 
 	printf( "Hello World!\n");
 }
