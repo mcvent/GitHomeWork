ba4f37a69bc55660568c33c640280c56e7a6ded6
mcvent <mcventik@yandex.ru>
2025-03-16 11:48:37 +0400

    added function fib

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 #include<stdio.h>
+
+int fib(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
 int main() {
+	int n = 10; 
+    	int result = fib(n); 
+    	printf("fib(%d) = %d\n", n, result); 
 	printf( "Hello World!\n");
 }
